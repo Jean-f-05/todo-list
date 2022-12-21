4adaa07a8ec05b40438ba76a9bdce04cf2d84b70
Jean-f-05 <jean_f_05@hotmail.com>
2022-12-21 15:45:36 +0000

    feat: added basic routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/", redirect: "/todos" },
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: "/todos",
+      name: null,
+      component: null,
+      children: [{ path: "add", component: null }],
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/todos/:id",
+      name: null,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import(""),
+      children: [
+        { path: "delete", component: null },
+        { path: "update", component: null },
+      ],
+    },
+    {
+      path: "/todos/add",
+      name: null,
+      component: null,
+    },
+    {
+      path: "/:notFound(.*)",
+      name: null,
+      component: null,
     },
   ],
 });
