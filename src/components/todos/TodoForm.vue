eaa945db4a3c13eb99d3704acecd14ae6807b5b5
Jean-f-05 <jean_f_05@hotmail.com>
2022-12-26 11:03:29 +0000

    feat: added todo form validations & styles

--- src/components/todos/TodoForm.vue
+++ src/components/todos/TodoForm.vue
@@ -1,9 +1,16 @@
 <template>
   <form action="" @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !description.isVaild }">
       <label for="description">Description</label>
-      <input type="text" id="description" v-model="description" />
+      <input
+        type="text"
+        id="description"
+        v-model="description.val"
+        @blur="clearValidation('description')"
+        @keydown="checkValidation('description')"
+      />
     </div>
+    <p v-if="!formIsValid">Form invalid! Please fix you input...</p>
     <base-button>ADD</base-button>
   </form>
 </template>
@@ -13,13 +20,34 @@ export default {
   emits: ["save-data"],
   data() {
     return {
-      description: "",
+      description: { val: "", isVaild: true },
+      formIsValid: true,
     };
   },
   methods: {
+    checkValidation(input) {
+      if (this[input].val.length >= 0) {
+        this[input].isVaild = true;
+      }
+    },
+    clearValidation(input) {
+      this[input].isVaild = true;
+    },
+    validateForm() {
+      this.formIsValid = true;
+      if (this.description.val.trim() === "") {
+        this.description.isVaild = false;
+        this.formIsValid = false;
+      }
+    },
     submitForm() {
+      this.validateForm();
+
+      if (!this.formIsValid) {
+        return;
+      }
       const formData = {
-        description: this.description,
+        description: this.description.val,
       };
 
       this.$emit("save-data", formData);
@@ -60,11 +88,18 @@ h3 {
   font-size: 1rem;
 }
 
+p {
+  margin-bottom: 15px;
+  margin-top: -5px;
+  color: #f03e3e;
+}
+
 .invalid label {
-  color: red;
+  color: #f03e3e;
 }
 
 .invalid input {
-  border: 1px solid red;
+  border: 1px solid #f03e3e;
+  background-color: #fcd8d8;
 }
 </style>
