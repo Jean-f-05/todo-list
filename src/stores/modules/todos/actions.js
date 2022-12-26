e5364173be9fd0eb290c2dd4b08fbc1acb4dcc7d
Jean-f-05 <jean_f_05@hotmail.com>
2022-12-26 11:46:18 +0000

    feat: added fetch api to submit new Todo (POST)

--- src/stores/modules/todos/actions.js
+++ src/stores/modules/todos/actions.js
@@ -1,11 +1,25 @@
 export default {
-  addTodo(context, data) {
+  async addTodo(context, data) {
     const todoData = {
-      id: 1,
       description: data.description,
       completed: false,
-      createdAt: new Date().toUTCString(),
     };
+
+    const response = await fetch(
+      "https://63650452f711cb49d1f2b256.mockapi.io/api/v1/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todoData),
+      }
+    );
+
+    if (!response.ok) {
+      //
+    }
+
     context.commit("addTodo", todoData);
   },
 };
