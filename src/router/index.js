498cd1723b2eb758e816898aa37bcbe7b524e130
Jean-f-05 <jean_f_05@hotmail.com>
2022-12-26 17:05:39 +0000

    fix: minor fix to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import TodoDetails from "./../pages/Todos/TodoDetails/TodoDetails.vue";
 import TodosList from "./../pages/Todos/TodosList/TodosList.vue";
 import AddTodo from "./../pages/Todos/AddTodo/AddTodo.vue";
 import TodoEdit from "./../pages/Todos/TodoEdit/TodoEdit.vue";
@@ -15,10 +14,13 @@ const router = createRouter({
       component: TodosList,
     },
     { path: "/add", component: AddTodo },
+
     {
-      path: "/todos/:id",
-      component: TodoDetails,
-      children: [{ path: "delete" }, { path: "edit", component: TodoEdit }],
+      path: "/todos/:id/delete",
+    },
+    {
+      path: "/todos/:id/edit",
+      component: TodoEdit,
     },
     {
       path: "/:notFound(.*)",
